f14ed266597dfe4b735929773719768443640cc2
NMSCSTR <rhondel.pagobo@nmsc.edu.ph>
2025-01-06 09:24:46 +0800

    setting up backend for registration

---
+++ api/authApiController.php
@@ -0,0 +1,45 @@
+<?php;
+header("Content-Type: application/json");
+include '../connection/db.php';
+
+
+$response = ['success' => false, 'message' => 'Invalid Request'];
+
+if ($_SERVER['REQUEST_METHOD' === 'POST']) {
+
+    $action = isset($_GET['action']) ? $_GET['action'] : '';
+    $data = json_decode(file_get_contents("php://input"), true);
+
+    if($action == 'register') {
+
+        $firstname = $data['firstname'] ?? '';
+        $lastname = $data['lastname'] ?? '';
+        $kcfapicode = $data['kcfapicode'] ?? '';
+        $email = $data['email'] ?? '';
+        $phone_number = $data['phone_number'] ?? '';
+        $role = $data['role'] ?? '';
+        $password = $data['password'] ?? '';
+        $password_confirmation = $data['password_confirmation'] ?? '';
+
+        if (empty($firstname) || empty($lastname) || empty($kcfapicode) || empty($email)|| empty($phone_number) || empty($role) || empty($password) || empty($password_confirmation)) {
+            $response['message'] = 'All fields are required.';
+        } elseif ($password !== $password_confirmation) {
+            $response['message'] = 'Password do not match.';
+        } else {
+            $hash_password = password_hash($password, PASSWORD_DEFAULT);
+            $sql = "INSERT INTO users (firstname, lastname, kcfapicode, email, phone_number, role, password)
+                        VALUES('$firstname','$lastname','$kcfapicode','$email','$phone_number','$role','$password)";
+
+            if (mysqli_query($conn, $sql)) {
+                $response['success'] = true;
+                $response['message'] = 'Registration successful';
+            }
+            else {
+                $response['message'] = 'Registration failed';
+            }
+        }
+
+    }
+
+    
+}
---
+++ connection/db.php
@@ -0,0 +1,15 @@
+<?php 
+define('BASE_URL', 'http://localhost/columbos/');
+
+$servername = "localhost";
+$username = "root";        
+$password = "";            
+$dbname = "columbos"; 
+
+$conn = mysqli_connect($servername, $username, $password, $dbname);
+
+if (!$conn) {
+    die("Connection failed: " . mysqli_connect_error());
+}
+
+?>
