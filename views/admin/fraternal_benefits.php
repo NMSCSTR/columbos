d1682ba7b65c2683468d4f0f4723f7daf19793ae
NMSCSTR <rhondel.pagobo@nmsc.edu.ph>
2025-01-13 19:41:03 +0800

    working with fraternal_benefits

---
+++ views/admin/fraternal_benefits.php
@@ -0,0 +1,42 @@
+<?php include '../../includes/admin_header.php'; ?>
+<title>Fraternal Benifits</title>
+<?php include '../../includes/admin_sidebar.php'; ?>
+
+<!-- Page Content -->
+
+<div class="container-fluid">
+    <div class="d-flex justify-content-start">
+        <button class="btn btn-success mb-3 shadow" type="button" data-bs-toggle="offcanvas"
+            data-bs-target="#staticBackdrop" aria-controls="staticBackdrop">
+            <i class="fas fa-plus"></i> Add Plan
+        </button>
+    </div>
+    <div class="card p-3">
+        <div class="container">
+            <div class="row">
+                <div class="col-sm">
+                    <div class="card" style="width: 18rem;">
+                        <img src="..." class="card-img-top" alt="...">
+                        <div class="card-body">
+                            <h5 class="card-title">Card title</h5>
+                            <p class="card-text">Some quick example text to build on the card title and make up the bulk
+                                of the card's content.</p>
+                            <a href="#" class="btn btn-primary">Go somewhere</a>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-sm">
+
+                </div>
+                <div class="col-sm">
+
+                </div>
+            </div>
+        </div>
+    </div>
+
+</div>
+<!-- /#page-content-wrapper -->
+
+
+<?php include '../../includes/admin_footer.php'; ?>
