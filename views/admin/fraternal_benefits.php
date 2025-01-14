236d9b78c70ba72ddf508c3cd14647ded30168b6
NMSCSTR <rhondel.pagobo@nmsc.edu.ph>
2025-01-14 17:12:37 +0800

    changing fraternal benifits card into table

--- views/admin/fraternal_benefits.php
+++ views/admin/fraternal_benefits.php
@@ -8,66 +8,69 @@
     <div class="d-flex justify-content-start">
         <button class="btn btn-success mb-3 shadow" type="button" data-bs-toggle="offcanvas"
             data-bs-target="#staticBackdrop" aria-controls="staticBackdrop">
-            <i class="fas fa-plus"></i> Add Savings Plan
+            <i class="fas fa-plus"></i> Add Plan
         </button>
     </div>
     <div class="card p-3 shadow">
-        <div class="container-fluid">
-            <div class="row">
-                <div class="col-sm">
-                    <div class="card shadow">
-                        <img src="<?php echo BASE_URL ?>resources/plans/1.GemPlan.png" class="card-img-top image-fluid"
-                            height="800" alt="...">
-                        <div class="card-body">
-                            <h5 class="card-title">Gem plans</h5>
-                            <p class="card-text">The KC Gem Savings Series Plans are
-                                endowment participating benefit
-                                certificate plans that provide cash
-                                benefits and level life insurance
-                                protection benefits.
-                            </p>
-                            <a href="#" class="btn btn-primary">More details</a>
-                        </div>
-                    </div>
-                </div>
-                <div class="col-sm">
-                    <div class="card shadow">
-                        <img src="<?php echo BASE_URL ?>resources/plans/2.DiamondPlan.png"
-                            class="card-img-top image-fluid" height="800" alt="...">
-                        <div class="card-body">
-                            <h5 class="card-title">Diamond plans</h5>
-                            <p class="card-text">The Guaranteed Insurance Offer (GIO)
-                                Avenger provides the assured a
-                                guaranteed family benefit protection
-                                before the contract's maturity date and a
-                                guaranteed lump sum payouts, if living
-                                are paid on specified date on or before
-                                the maturity date
+    <div class="table-responsive">
+            <table id="example" class="table table-bordered  table-sm display responsive nowrap caption-top"
+                style="width:100%">
+                <caption><i class="fas fa-list"></i> List of users</caption>
+                <hr class="border border-primary border-3 opacity-75">
+                <thead class="table-dark">
+                    <tr>
+                        <th>STATUS</th>
+                        <th>FIRSTNAME</th>
+                        <th>LASTNAME</th>
+                        <th>KCFAPICODE</th>
+                        <th>EMAIL</th>
+                        <th>PHONE NUMBER</th>
+                        <th>ROLE</th>
+                        <th>ACTIONS</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    <?php
+                        $fetch_users = mysqli_query($conn, "SELECT * FROM users WHERE role != 'admin'");
+                        while ($user = mysqli_fetch_assoc($fetch_users)) { ?>
 
-                            </p>
-                            <a href="#" class="btn btn-primary">More details</a>
-                        </div>
-                    </div>
-                </div>
-                <div class="col-sm">
-                    <div class="card shadow">
-                        <img src="<?php echo BASE_URL ?>resources/plans/3.GioAvenger.png"
-                            class="card-img-top image-fluid" height="800" alt="...">
-                        <div class="card-body">
-                            <h5 class="card-title">Geo Avenger</h5>
-                            <p class="card-text">The Guaranteed Insurance Offer (GIO)
-                                Crusader provides the assured a
-                                guaranteed death benefit protection
-                                before the contract's maturity date and a
-                                guaranteed lump sum, if living, is paid on
-                                the maturity date
+                    <tr>
+                        <td><span  style="width: 120px; height: 40px; display: flex; justify-content: center; align-items: center; text-transform: uppercase;"
+                                class="badge <?php echo $user['status'] == 'approved' ? 'text-bg-success shadow p-2' : ($user['status'] == 'pending' ? 'text-bg-warning shadow p-2' : ''); ?>">
+                                <?php echo strtoupper($user['status']); ?>
+                            </span>
+                        </td>
+                        <td><?php echo $user['firstname'] ?></td>
+                        <td><?php echo $user['lastname'] ?></td>
+                        <td><?php echo $user['kcfapicode'] ?></td>
+                        <td><?php echo $user['email'] ?></td>
+                        <td><?php echo $user['phone_number'] ?> <span><i class="fas fa-message float-end text-primary"></i></span></td>
+                        <td><?php echo ucfirst($user['role']) ?></td>
 
-                            </p>
-                            <a href="#" class="btn btn-primary">More details</a>
-                        </div>
-                    </div>
-                </div>
-            </div>
+                        <td>
+                            <button onclick="deleteUser(<?php echo $user['id']; ?>)" class="btn btn-danger btn-sm">
+                                <i class="fas fa-trash-alt"></i> Delete
+                            </button>
+                            <button onclick="approvedUser(<?php echo $user['id']; ?>)" class="btn btn-success btn-sm">
+                                <i class="fas fa-check"></i> Approve
+                            </button>
+                        </td>
+                    </tr>
+                    <?php } ?>
+                <tfoot>
+                    <tr>
+                        <th>STATUS</th>
+                        <th>FIRSTNAME</th>
+                        <th>LASTNAME</th>
+                        <th>KCFAPICODE</th>
+                        <th>EMAIL</th>
+                        <th>PHONE NUMBER</th>
+                        <th>ROLE</th>
+                        <th>ACTIONS</th>
+                    </tr>
+                </tfoot>
+                </tbody>
+            </table>
         </div>
     </div>
 
